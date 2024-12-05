4eda6b4ea583ab1045db26b87bbfbe5bbb2a496a
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2024-12-05 12:29:57 -0500

    check for differences between areas, can teleport

--- home.js
+++ home.js
@@ -1,23 +1,26 @@
 'use strict';
 
 class Home extends Wall {
-    constructor(pos, size, owner, furniture_data, floor_plan, outside_floor_plan, exterior, door) {
+    constructor(pos, size, owner, furniture_data, floor_plan, outside_floor_plan, exterior, home_pos=vec2(-20,-20)) {
         super(pos, size, "home");
 
-        this.owner = owner;
-        this.locked = true;
-        this.furniture_data = furniture_data;
-        this.floor_plan = floor_plan;
-        this.outside_floor_plan = outside_floor_plan;
-        this.exterior = exterior;
-        this.door = door;
+        this.home_info = {
+            owner: owner,
+            locked: false, // remember to unlock lock this please
+            furniture_data: furniture_data,
+            floor_plan: floor_plan,
+            outside_floor_plan: outside_floor_plan,
+            exterior: exterior,
+            home_pos: home_pos,
+        }
+        this.area = new InteractArea(vec2(pos.x, pos.y - 2), vec2(1,1), this.home_info, "entering_area");
     }
 
     unlockHome() {
-        this.locked = false;
+        this.home_info.locked = false;
     }
 
     lockHome() {
-        this.locked = true;
+        this.home_info.locked = true;
     }
 }
--- player.js
+++ player.js
@@ -9,8 +9,10 @@ class Player extends EngineObject {
         this.damping = 0.95;
         this.talking = false;
         this.current_convo = "";
+        this.in_interact_area = false;
         this.found_info = "";
         this.name = name;
+        this.pause = false;
         this.talked_with = {
             // Get villager names "Bob": 2 times today, get convos
             // introduced : true/ false or -1 on the talked with. reset to 0 when introduced and new day starts if haven't introduced and day resets, stay at -1
@@ -53,10 +55,12 @@ class Player extends EngineObject {
                 if (this.velocity.direction() == 0) { // TODO fix this
                     this.in_interact_area = true;
                     this.found_info = obj.info;
+                    this.in_area = obj.subtype;
                 }
                 else {
                     this.in_interact_area = false;
                     this.found_info = "";
+                    this.in_area = "";
                 }
                 
             }
@@ -64,43 +68,55 @@ class Player extends EngineObject {
                 if (this.velocity.direction() == 0) { // TODO fix this
                     this.in_interact_area = true;
                     this.found_info = obj.info;
+                    this.in_area = obj.subtype;
                 }
                 else {
                     this.in_interact_area = false;
                     this.found_info = "";
+                    this.in_area = "";
                 }
             }
 
             return false;
         }
 
-
-
         return true;
     }
 
     interact() {
         if (keyWasPressed('Enter')) {
-            if (this.current_convo !== "") {
-                let movedText = this.current_convo.moveText();
-                if (!movedText) {
-                    this.current_convo.destroy();
-                    this.current_convo = "";
-                    this.found_info = "";
-                    this.talking = false;
+            if (this.in_area == "talking_area") {
+                if (this.current_convo !== "") {
+                    let movedText = this.current_convo.moveText();
+                    if (!movedText) {
+                        this.current_convo.destroy();
+                        this.current_convo = "";
+                        this.found_info = "";
+                        this.talking = false;
+                    }
+                }
+                else {
+                    this.talking = true;
+                    this.talk(this.found_info.name, this.found_info.talk_type);
                 }
             }
-            else {
-                this.talking = true;
-                this.talk(this.found_info.name, this.found_info.talk_type);
+            else if (this.in_area == "entering_area") {
+                this.enterArea();
             }
         }
     }
 
-    enterHouse(obj) {
-        if (!obj.locked && keyIsDown('Enter')) {
-            moveToHouse(obj.name, obj);
+    enterArea() {
+        if (!this.found_info.locked) {
+            this.teleport(this.found_info.home_pos); // consider where to start
         }
+    }
+
+    teleport(pos) {
+        // this.pause = true;
+        this.velocity = vec2(0,0);
+        this.pos = pos;
+        // this interact area = false
     }
 
     talk(villager_name, talk_type) {
